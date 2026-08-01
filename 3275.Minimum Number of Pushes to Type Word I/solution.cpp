6baa65fc3e75a2a6ce8360d1341b739b4cb6890e
Nitesh@4546 <niteshnunia91@gmail.com>
2026-08-02 00:01:33 +0530

    3275.Minimum Number of Pushes to Type Word I

---
+++ 3275.Minimum Number of Pushes to Type Word I/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int push = 0;
+        int n = word.size();
+        for(int i = 0; i < n; i++) {
+            push += (i / 8) + 1;
+        }
+        return push;
+    }
+};
