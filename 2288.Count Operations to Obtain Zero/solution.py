4845d270e370d523d5ee4175532f8ec750c83dc5
Nitesh@4546 <niteshnunia91@gmail.com>
2026-08-07 14:26:46 +0530

    2288.Count Operations to Obtain Zero

---
+++ 2288.Count Operations to Obtain Zero/solution.py
@@ -0,0 +1,14 @@
+#Enjoy the question
+class Solution(object):
+    def countOperations(self, num1, num2):
+        """
+        :type num1: int
+        :type num2: int
+        :rtype: int
+        """
+        c = 0
+        while(num1!=0 and num2!=0):
+            if num1>=num2:num1-=num2
+            else:num2-=num1
+            c+=1
+        return c
