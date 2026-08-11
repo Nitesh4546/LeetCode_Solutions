5d6b135f23b65c8020b16a061edd736338c07253
Nitesh@4546 <niteshnunia91@gmail.com>
2026-08-11 20:16:01 +0530

    191.Number of 1 Bits

---
+++ 191.Number of 1 Bits/solution.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        c = 0
+        for i in bin(n)[2:]:
+            if i=="1":
+                c+=1
+        return c
