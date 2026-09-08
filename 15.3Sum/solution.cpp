76f04cd16e6ffa088c68e27d373430daa1cab956
Nitesh@4546 <niteshnunia91@gmail.com>
2026-09-08 23:09:29 +0530

    15.3Sum

---
+++ 15.3Sum/solution.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         for(int k=0;k<n;k++){
+        //             if(i==j || i==k || j==k){
+        //                 continue;
+        //             }
+        //             if(nums[i]+nums[j]+nums[k]==0){
+        //                 res.push_back({nums[i],nums[j],nums[k]});
+        //             }
+        //         }
+        //     }
+        // }
+        int k = n - 1;
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
+            int j = i+1;
+            int k = n-1;
+            while(j<k){
+
+            
+            int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    res.push_back({nums[i], nums[j], nums[k]});
+                    // j++;
+                    // break;
+                    //duplicacy
+                    while(j<k && nums[j]==nums[j+1]){
+                        j++;
+                    }
+                    while(j<k &&  nums[k]==nums[k-1]){
+                        k--;
+                    }
+                    k--;
+
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    j++;
+                }
+            }
+        }
+        return res;
+    }
+};
