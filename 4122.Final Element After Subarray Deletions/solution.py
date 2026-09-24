a483fcfe45f53e46cafe018c2e2ad3b337a8d367
Nitesh@4546 <niteshnunia91@gmail.com>
2026-09-24 19:49:12 +0530

    4122.Final Element After Subarray Deletions

---
+++ 4122.Final Element After Subarray Deletions/solution.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def finalElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return max(nums[0],nums[-1])
+        
